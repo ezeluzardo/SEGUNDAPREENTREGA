737dcebf5c74f3d4baf7cb8a8d501f234884d034
ezeluzardo <ezeluzardo@gmail.com>
2023-09-15 22:17:43 -0300

    1
    1

--- js.js
+++ js.js
@@ -1,41 +1,63 @@
-
-const resultados = [];
-
-
-function realizarOperacion(operacion) {
-    const numero1 = parseFloat(prompt('Ingrese el primer número:'));
-    const numero2 = parseFloat(prompt('Ingrese el segundo número:'));
-
-    let resultado;
-
-    switch (operacion) {
-        case 'suma':
-            resultado = numero1 + numero2;
-            break;
-        case 'resta':
-            resultado = numero1 - numero2;
-            break;
-        case 'multiplicacion':
-            resultado = numero1 * numero2;
-            break;
-        case 'division':
-            resultado = numero1 / numero2;
-            break;
-        case 'porcentaje':
-            resultado = (numero1 * numero2) / 100;
-            break;
-        default:
-            console.log('Operación no válida.');
-            return;
+let tof = true;
+class Producto {
+    constructor(nombre, precio, cantidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
     }
 
-
-    resultados.push(resultado);
-
-
-    console.log(`Resultado de ${operacion}: ${resultado}`);
+    obtenerInformacion() {
+        return `Nombre: ${this.nombre}, Precio: $${this.precio}, Cantidad: ${this.cantidad}`;
+    }
 }
 
+const productos = [];
 
-const resultadoMayorA10 = resultados.filter(resultado => resultado > 10);
-console.log('Resultados mayores a 10:', resultadoMayorA10);
+function agregarProducto() {
+    const nombre = prompt('Ingrese el nombre del producto:');
+    const precio = parseFloat(prompt('Ingrese el precio del producto:'));
+    const cantidad = parseInt(prompt('Ingrese la cantidad del producto:'));
+
+    const producto = new Producto(nombre, precio, cantidad);
+    productos.push(producto);
+}
+
+function mostrarProductos() {
+    console.log('Lista de productos:');
+    productos.forEach((producto, index) => {
+        console.log(`${index + 1}. ${producto.obtenerInformacion()}`);
+    });
+}
+
+function ordenarProductos() {
+    productos.sort((a, b) => a.nombre.localeCompare(b.nombre));
+}
+
+while (tof) {
+    const opcion = prompt(
+        'Seleccione una opción:\n1. Agregar producto\n2. Mostrar productos\n3. Ordenar productos por nombre\n4. Salir'
+    );
+
+    switch (opcion) {
+        case '1':
+            agregarProducto();
+            console.log('Producto agregado.');
+            break;
+        case '2':
+            mostrarProductos();
+            console.log('Productos mostrados.');
+            break;
+        case '3':
+            ordenarProductos();
+            console.log('Productos ordenados por nombre.');
+            break;
+        case '4':
+            console.log('Saliendo del programa.');
+            alert('Gracias por usar el programa.');
+            tof = false;
+            // Puedes agregar más opciones o funcionalidades aquí si lo deseas.
+            break;
+        default:
+            console.log('Opción no válida. Por favor, seleccione una opción válida.');
+    }
+}
